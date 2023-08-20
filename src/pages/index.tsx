e286b060b86d8538833d8c638730d48aa0fd285e
s <11375404+Shreyaan@users.noreply.github.com>
2023-08-21 02:24:27 +0530

    works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,60 +7,51 @@ import { useEffectOnce } from "usehooks-ts";
 
 function getCorrectShlokaNumber(currentShlokaNum: number): [number, number] {
   const ranges = [
-    46, 71, 42, 41, 28, 46, 29, 27, 33, 41, 54, 19, 34, 26, 19, 23, 27, 77,
+    47, 72, 43, 42, 29, 47, 30, 28, 34, 42, 55, 20, 35, 27, 20, 24, 28, 78,
   ];
   let offset = 0;
 
   for (let i = 0; i < ranges.length; i++) {
     if (currentShlokaNum <= offset + ranges[i]) {
-      return [i + 1, currentShlokaNum - offset];
+      return [i + 1, currentShlokaNum - offset - 1];
     }
     offset += ranges[i];
   }
 
-  return [1, 1];
+  return [0, 0];
 }
 
 const IndexPage = () => {
-  const [data, setData] = React.useState<any>([]);
+  const [data, setData] = React.useState([1, 2, 3, 4, 5, 6]);
   const [globalShlokaNumber, setGlobalShlokaNumber] = React.useState(0);
 
-  useEffectOnce(() => {
-    let firstfive = [] as Data[];
-    firstFiveLoop().then(() => {
-      setData(firstfive);
-    });
+  // useEffectOnce(() => {
 
-    async function firstFiveLoop() {
-      for (let i = 1; i <= 1; i++) {
-        for (let j = 0; j <= 5; j++) {
-          const data: Data = {
-            chapterNumber: getCorrectShlokaNumber(j)[0],
-            shlokaNumber: getCorrectShlokaNumber(j)[1],
-          };
-          firstfive.push(data);
-          await setGlobalShlokaNumber(globalShlokaNumber + 1);
-          console.log(globalShlokaNumber);
-        }
-      }
-    }
-  });
+  //     setGlobalShlokaNumber(5);
 
-  // useEffect(() => {
-  //   const dataobj: Data = {
-  //     chapterNumber: getCorrectShlokaNumber(globalShlokaNumber)[0],
-  //     shlokaNumber: getCorrectShlokaNumber(globalShlokaNumber)[1],
+  // });
+
+  // console.log(globalShlokaNumber + " globalShlokaNumber");
+  // let array: Data[] = [];
+
+  // for (let i = 0; i <= globalShlokaNumber; i++) {
+  //   const data: Data = {
+  //     chapterNumber: getCorrectShlokaNumber(i)[0],
+  //     shlokaNumber: getCorrectShlokaNumber(i)[1],
   //   };
-  //   setData([...data, data]);
-  // }, [data, globalShlokaNumber]);
+  //   array.push(data);
+  // }
 
   return (
     <div className="bg-black min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-white text-4xl mb-16">Gita SuperSite Revamped</h1>
-      {data.map((item: Data, index: React.Key | null | undefined) => (
+      {data.map((item, index: React.Key | null | undefined) => (
         <Shloka
           key={index}
-          Shlokadata={item}
+          Shlokadata={{
+            chapterNumber: getCorrectShlokaNumber(item)[0],
+            shlokaNumber: getCorrectShlokaNumber(item)[1],
+          }}
           dataArr={data}
           setData={setData}
         />
